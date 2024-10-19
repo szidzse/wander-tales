5ccdfbd1e7b129d4c646bb0900d8e41a7dbcc2c7
szidzse <gellertsos@gmail.com>
2024-10-19 17:24:00 +0200

    install react router, create base paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 
 const App = () => {
-  return <div>App</div>;
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route path="/dashboard" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 };
 
 export default App;
