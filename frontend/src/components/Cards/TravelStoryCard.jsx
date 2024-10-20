595442cc062b11b8e0d8e65d3ee654ac6a8afa03
szidzse <gellertsos@gmail.com>
2024-10-20 17:46:49 +0200

    create TravelStoryCard

---
+++ frontend/src/components/Cards/TravelStoryCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TravelStoryCard = ({ }) => {
+  return (
+    <div>
+        <h1>travel story card</h1>
+    </div>
+  )
+};
+
+export default TravelStoryCard;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
+import TravelStoryCard from "../../components/Cards/TravelStoryCard";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 
 const Home = () => {
 	const [userInfo, setUserInfo] = useState({});
+	const [allStories, setAllStories] = useState([]);
 
 	const navigate = useNavigate();
+
+	const getAllTravelStories = async () => {
+		try {
+			const response = await axiosInstance.get("/get-all-travel-stories");
+			if (response.data && response.data.stories) {
+				setAllStories(response.data.stories);
+			}
+		} catch (error) {
+			console.log("An unexpected error occurred. Please try again.");
+		}
+	};
 
 	const getUserInfo = async () => {
 		try {
@@ -23,15 +36,33 @@ const Home = () => {
 	};
 
 	useEffect(() => {
+		getAllTravelStories();
 		getUserInfo();
 
 		return () => {};
 	}, []);
 
 	return (
-		<div>
+		<>
 			<Navbar userInfo={userInfo} />
-		</div>
+
+			<div className="container mx-auto py-10">
+				<div className="flex gap-7">
+					<div className="flex-1">
+						{allStories.length > 0 ? (
+							<div className="grid grid-cols-2 gap-4">
+								{allStories.map((item) => {
+									return <TravelStoryCard key={item._id} />;
+								})}
+							</div>
+						) : (
+							<>Empty Card Here</>
+						)}
+						<div className="w-[320px]"></div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
