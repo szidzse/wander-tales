a3790aa9ad293b334f676200e124315161ec6f2e
szidzse <gellertsos@gmail.com>
2024-10-20 15:05:59 +0200

    implement SignUp page and it's functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
@@ -9,6 +9,7 @@ const App = () => {
 		<div>
 			<Router>
 				<Routes>
+					<Route path="/" element={<Root />} />
 					<Route path="/dashboard" element={<Home />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/signup" element={<SignUp />} />
@@ -16,6 +17,12 @@ const App = () => {
 			</Router>
 		</div>
 	);
+};
+
+const Root = () => {
+	const isAuthenticated = !!localStorage.getItem("token");
+
+	return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
 };
 
 export default App;
