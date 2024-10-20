bebe47422622c07da5aa0e9ae6aee5d2547d4e07
szidzse <gellertsos@gmail.com>
2024-10-20 11:28:56 +0200

    implement search travels

--- backend/server.js
+++ backend/server.js
@@ -286,6 +286,30 @@ app.put("/update-is-favorite/:id", authenticateToken, async (req, res) => {
 	}
 });
 
+app.get("/search", authenticateToken, async (req, res) => {
+	const { query } = req.query;
+	const { userId } = req.user;
+
+	if (!query) {
+		return res.status(404).json({ error: true, message: "Query is required." });
+	}
+
+	try {
+		const searchResults = await TravelStory.find({
+			userId: userId,
+			$or: [
+				{ title: { $regex: query, $options: "i" } },
+				{ story: { $regex: query, $options: "i" } },
+				{ visitedLocation: { $regex: query, $options: "i" } },
+			],
+		}).sort({ isFavorite: -1 });
+
+		res.status(200).json({ stories: searchResults });
+	} catch (error) {
+		res.status(500).json({ error: true, message: error.message });
+	}
+});
+
 app.listen(PORT, async () => {
 	console.log(`Server is running on  http://localhost:${PORT}`);
 });
